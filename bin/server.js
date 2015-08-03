4b0090a3c52454f97a3dccab59feb57e9e0974ca
Hiroaki Yamamoto <admin@hysoftware.net>
2015-08-03 13:22:57 +0900

    Fixed server.js was still 0.12

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,15 @@
 /*global require, process*/
 (function (r, p) {
     "use strict";
-    var server = r("karma").server,
+    var Server = r("karma").Server,
         config = [];
 
     p.stdin.on("readable", function () {
         config.push(p.stdin.read());
     });
     p.stdin.on("end", function () {
+        var server = new Server(config);
         config = JSON.parse(config.join(""));
-        server.start(config);
+        server.start();
     });
 }(require, process));
